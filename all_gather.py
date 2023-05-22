336f2ebbfa9849667cbb11d0dcb786d2c3a4bb02
Rui Chen <rui.chen@avalara.com>
2023-05-22 17:10:44 +0000

    working sample for all_gather

---
+++ all_gather.py
@@ -0,0 +1,25 @@
+# run the script:
+# torchrun --nproc_per_node=8 --nnode=1 all_gather.py 
+
+import os
+import torch
+import torch.distributed as dist
+
+def main():
+    rank = int(os.getenv('RANK', '0'))
+    world_size = int(os.getenv('WORLD_SIZE', '1'))
+    local_rank = int(os.getenv('LOCAL_RANK', '0'))
+
+    print(rank, world_size, local_rank)
+
+    torch.cuda.set_device(local_rank)
+    dist.init_process_group(backend='nccl')
+
+    t = torch.rand(1).cuda(local_rank)
+    gather_t = [torch.ones_like(t) for _ in range(world_size)]
+    dist.all_gather(gather_t, t)
+    if rank == 0:
+        print(rank, t, gather_t)
+
+if __name__ == '__main__':
+    main()
